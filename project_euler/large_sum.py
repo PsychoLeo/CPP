18d6451022743a8f303e80a76c749b9a19947e0f
PsychoLeo <leopold.bernard.leo@gmail.com>
2021-04-07 20:04:12 +0200

    moved project euler folder to Algorithms

--- project_euler/large_sum.py
+++
@@ -1,5 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-l = [int(input().strip()[:12]) for _ in range (100)]
-print(str(sum(l))[:10])
--- project_euler/pythagorean_triplet.py
+++
@@ -1,8 +0,0 @@
-from math import sqrt
-
-for b in range (1000) :
-    for a in range (b) :
-        if a + b + sqrt(a*a+b*b) == 1000 :
-            c = sqrt(a**2 + b**2)
-            if a < b < c :
-                print(a*b*c)
