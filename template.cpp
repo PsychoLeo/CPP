80ee5ae177df87f385156a47b1782c2c4eb2fbbf
PsychoLeo <leopold.bernard.leo@gmail.com>
2021-04-23 14:57:56 +0200

    template & exercices

---
+++ template.cpp
@@ -0,0 +1,61 @@
+// Including
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <unordered_set>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <fstream>
+#include <bitset>
+#include <climits>
+
+using namespace std;
+
+// STL functions
+#define pb push_back
+#define mt make_tuple
+#define mp make_pair
+#define f first
+#define s second
+
+// Iteration
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define fo(i,n) for(int i=0; i<n; i++)
+
+// Input and output
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x) 
+#define ss(x) getline(cin, x)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(x) cout << x << "\n"
+
+// Types 
+#define ll long long
+#define ld long double
+#define str string
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+
+// Debugging
+#define isDebug true
+#ifdef isDebug
+#define debug(x) cout << #x << "=" << x << "\n"
+#else
+#define debug(x)
+#endif
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	return 0;
+}
