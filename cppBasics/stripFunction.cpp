2142150e07fae8af74abf73b631dc4d1f945d494
PsychoLeo <leopold.bernard.leo@gmail.com>
2021-05-20 22:41:54 +0200

    c++ folder

--- cppBasics/stripFunction.cpp
+++ cppBasics/stripFunction.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
-int main() {
+string strip(string s) {
+    auto pLeft = s.begin();
+    while (isspace(*pLeft)) {
+        pLeft++;
+    }
+    s.erase(s.begin(), pLeft);
+    auto pRight = s.end()-1;
+    while (isspace(*pRight)) {
+        pRight--;
+    }
+    s.erase(++pRight, s.end());
+    return s;
+}
 
+int main() {
+    string s;
+    getline(cin, s);
+    cout << "Before strip : " << s << endl;
+    s = strip(s);
+    cout << "After strip : " << s << endl;
+    return 0;
 }
